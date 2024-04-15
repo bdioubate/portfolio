71de33beeb17eae77fe2bc33a8a19fed96b2c56a
bdioubate <bangalidioubate@gmail.com>
2024-04-15 17:34:57 +0200

    Modify DarkModeToggle component

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -7,29 +7,20 @@ import sunIcon from "../assets/icons/sun.svg";
 
 interface ToggleDarkModeProps {}
 
-//redux
-import { useSelector } from 'react-redux'
-interface RootState {
-  darkMode: {
-      mode: boolean;
-  };
-}
-
 const DarkModeToggle = ({}: ToggleDarkModeProps) => {
-  const { mode } = useSelector((state: RootState) => state.darkMode);
   const dispatch = useDispatch();
-  const [mode1, setMode1] = useState<boolean>(false);
+  const [mode, setMode] = useState<boolean>(false);
 
   const handleToggleDarkMode = () => {
-    const newMode1 = !mode1;
-    setMode1(newMode1);
-    localStorage.setItem("darkMode", JSON.stringify(newMode1));
+    const newMode = !mode;
+    setMode(newMode);
+    localStorage.setItem("darkMode", JSON.stringify(newMode));
     dispatch(toggleDarkMode()); 
   };
 
   return (
     <button id="dark_mode" onClick={handleToggleDarkMode}>
-      <Image fill={true} src={ mode1 ? sunIcon.src : moonIcon.src} alt="L'icone du darkmode" /> 
+      <Image fill={true} src={ mode ? sunIcon.src : moonIcon.src} alt="L'icone du darkmode" /> 
     </button> 
   );
 };
